902353787e236eca3f4651a7641e21d8fb8c8b2a
Longseax <91423164+YaroslavPryatkin@users.noreply.github.com>
2026-06-05 15:50:41 +0200

    Added new gui in new branch
    It now has drop downs

--- GUI/src/main/java/com/postgres_gui/Main.java
+++
@@ -1,7 +0,0 @@
-package com.postgres_gui;
-
-public class Main {
-    public static void main(String[] args) {
-        GuiApp.main(args);
-    }
-}
--- GUI/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.postgres_gui {
-    requires javafx.controls;
-    requires java.sql;
-
-    exports com.postgres_gui;
-}
